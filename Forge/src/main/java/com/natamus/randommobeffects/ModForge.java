16bb8670a27e82cbfff5fc1b8bb17e5d14597e08
Rick South <rick@natamus.com>
2023-12-12 00:23:19 +0100

    Random Mob Effects source code for 1.19.2.

---
+++ Forge/src/main/java/com/natamus/randommobeffects/ModForge.java
@@ -0,0 +1,45 @@
+package com.natamus.randommobeffects;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.randommobeffects.forge.config.IntegrateForgeConfig;
+import com.natamus.randommobeffects.forge.events.ForgeAddEffectEvent;
+import com.natamus.randommobeffects.util.Reference;
+import com.natamus.randommobeffects.util.Util;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.io.IOException;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	try {
+	    	if (Util.setupPotionEffects()) {
+	    		MinecraftForge.EVENT_BUS.register(new ForgeAddEffectEvent());
+	    	}
+		} catch (IOException ex) {
+			System.out.println("[" + Reference.NAME + "] Something went wrong while setting up the list of potion effects.");
+		}
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
