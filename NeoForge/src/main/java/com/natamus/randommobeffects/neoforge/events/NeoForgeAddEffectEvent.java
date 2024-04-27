db47032c7586bd3abd465abb6313670a5d6a24ff
Rick South <rick@natamus.com>
2024-04-28 01:28:48 +0200

    Random Mob Effects source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/randommobeffects/neoforge/events/NeoForgeAddEffectEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.randommobeffects.neoforge.events;
+
+import com.natamus.randommobeffects.events.AddEffectEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeAddEffectEvent {
+	@SubscribeEvent
+	public static void onMobSpawn(EntityJoinLevelEvent e) {
+		AddEffectEvent.onMobSpawn(e.getLevel(), e.getEntity());
+	}
+}
