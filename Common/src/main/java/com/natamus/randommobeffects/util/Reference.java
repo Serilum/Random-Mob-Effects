ba9c55524e77bd02dad5c9be2120085e611f2d9a
Rick South <rick@natamus.com>
2024-05-01 16:32:54 +0200

    Random Mob Effects source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/randommobeffects/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.randommobeffects.util;
+
+public class Reference {
+	public static final String MOD_ID = "randommobeffects";
+	public static final String NAME = "Random Mob Effects";
+	public static final String VERSION = "3.3";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
---
+++ NeoForge/src/main/java/com/natamus/randommobeffects/ModNeoForge.java
@@ -0,0 +1,44 @@
+package com.natamus.randommobeffects;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.randommobeffects.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.randommobeffects.neoforge.events.NeoForgeAddEffectEvent;
+import com.natamus.randommobeffects.util.Reference;
+import com.natamus.randommobeffects.util.Util;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.io.IOException;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		try {
+			if (Util.setupPotionEffects()) {
+				NeoForge.EVENT_BUS.register(NeoForgeAddEffectEvent.class);
+			}
+		} catch (IOException ex) {
+			System.out.println("[" + Reference.NAME + "] Something went wrong while setting up the list of potion effects.");
+		}
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
