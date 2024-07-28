eaed30359f0919fb8fc6e410129a4d481dd96c41
Rick South <rick@natamus.com>
2024-07-28 07:18:45 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/randommobeffects/ModFabric.java
+++ Fabric/src/main/java/com/natamus/randommobeffects/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.randommobeffects;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.randommobeffects.events.AddEffectEvent;
 import com.natamus.randommobeffects.util.Reference;
 import com.natamus.randommobeffects.util.Util;
@@ -15,6 +16,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
