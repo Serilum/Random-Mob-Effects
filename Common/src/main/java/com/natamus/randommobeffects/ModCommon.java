62692fa7982e810222ab40862dac117c48c8d0aa
Rick South <rick@natamus.com>
2026-03-26 01:33:23 +0100

    Random Mob Effects source code for 26.1.0.

---
+++ Common/src/main/java/com/natamus/randommobeffects/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.randommobeffects;
+
+import com.natamus.randommobeffects.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+
+	}
+}
